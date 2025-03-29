7360d773263467626b0ce9ca9275563e35a9bf10
Gourav Sen <sen9165812790@gmail.com>
2025-03-29 10:51:35 +0530

    Update forms.py

--- face_recognition/forms.py
+++ face_recognition/forms.py
@@ -5,3 +5,10 @@ class StudentRegistrationForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['student_id', 'name', 'email', 'phone', 'branch']
+        widgets = {
+            'student_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'branch': forms.TextInput(attrs={'class': 'form-control'}),
+        }
